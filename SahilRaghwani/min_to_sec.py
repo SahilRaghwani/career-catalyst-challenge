0c2aa26b99c6857316193a15897294a901aaf413
SahilRaghwani <101628758+SahilRaghwani@users.noreply.github.com>
2022-03-17 14:53:50 +0300

    Add files via upload

---
+++ SahilRaghwani/min_to_sec.py
@@ -0,0 +1,10 @@
+#Sahil
+def timeConverison(number):
+    timeSeconds = number * 60
+    
+    print ("\nThe number of minutes are: \n", number)
+    print("The timeConversion in seconds is: \n", timeSeconds)
+      
+#Number in minutes    
+number = 2
+timeConverison(number); 
